28d487afa29ed8948f2108fd882634f15926a099
Muhamad Yusup <muhamad.yusup@muf.co.id>
2026-08-27 10:59:10 +0700

    [FIXING] menyesuaikan service userApp

--- src/main/java/com/book_catalog_web/repository/AppUserRepository.java
+++ src/main/java/com/book_catalog_web/repository/AppUserRepository.java
@@ -2,6 +2,8 @@ package com.book_catalog_web.repository;
 
 import com.book_catalog_web.domain.AppUser;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,11 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     @Modifying
     @Query("UPDATE AppUser a SET a.isDeleted = true WHERE a.id = :id")
     public void softDelete(Long id);
+
+    @Query("""
+        SELECT u FROM AppUser u WHERE u.name LIKE LOWER(:userName)
+                OR u.email LIKE LOWER(:email)
+                OR u.mobileNumber LIKE LOWER(:mobileNumber)
+        """)
+    public Page<AppUser> findAllUser(String userName, String email, String mobileNumber, Pageable pageable);
 }
--- src/main/java/com/book_catalog_web/service/Impl/UserServiceImpl.java
+++ src/main/java/com/book_catalog_web/service/Impl/UserServiceImpl.java
@@ -9,10 +9,13 @@ import com.book_catalog_web.repository.AppUserRepository;
 import com.book_catalog_web.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,10 +48,14 @@ public class UserServiceImpl implements UserService {
         Pageable pageable = PageRequest.of(pages, limit, sort);
 
         userName = StringUtils.isBlank(userName) ? "%" : "%" + userName + "%";
+        email = StringUtils.isBlank(email) ? "%": "%" + email + "%" ;
+        mobileNumber = StringUtils.isBlank(mobileNumber) ? "%" : "%" + mobileNumber + "%" ;
 
+        Page<AppUser> userPage = appUserRepository.findAllUser(userName, email, mobileNumber, pageable);
 
+        List<UserListResponseDTO> usersDTO = userPage.stream().map(u -> new UserListResponseDTO(u.getId(), u.getSecureId(), u.getName())).toList();
 
-        return null;
+        return new ResultPageResponseDTO<>(usersDTO, userPage.getTotalPages(), userPage.getTotalElements());
     }
 
     @Override
